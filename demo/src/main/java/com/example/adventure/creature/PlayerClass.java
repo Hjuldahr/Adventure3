75361be3616215548c11dc58d953c0a2656defd0
Hjuldahr <rjohly@gmail.com>
2026-03-27 14:50:01 -0400

    ...

--- demo/src/main/java/com/example/adventure/creature/PlayerClass.java
+++ demo/src/main/java/com/example/adventure/creature/PlayerClass.java
@@ -7,13 +7,13 @@ import com.example.adventure.combat.DamageTypes;
 import com.example.adventure.randomizer.Dice;
 
 public class PlayerClass {
-    
+    private EnumMap<DamageTypes, DamageModifierCategories> damageAdjustments;
 
     public Dice getHitDiceForLevel(int level) {
         throw new UnsupportedOperationException("Unimplemented");
     }
 
     public EnumMap<DamageTypes,DamageModifierCategories> getDamageAdjustments() {
-        return damageAdjustments;
+        return this.damageAdjustments;
     }
 }
